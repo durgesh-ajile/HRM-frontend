163b3eb647de0c5bfc7f0b39fdfbebbe7ea1e651
durgesh-ajile <durgesh.rajak@ajiledone.com>
2023-08-03 18:10:29 +0530

    added sidebar

---
+++ Landing.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+// import ReactBigCalendar from "./src/Pages/Calenders/Calender";
+import Profile from "./src/Component/profile/Profile";
+import ResponsiveDrawer from "./src/Component/Sidebar/Sidebar";
+// import ContractorForm from "./src/Pages/ContractorForm/ContractorForm";
+import AdminContractorTab from "./src/Pages/AdminContractorTab/AdminContractorTab";
+
+const Landing = () => {
+  return (
+    <div className="landing">
+      <ResponsiveDrawer />
+      <div className="right-container">
+        <Routes>
+          <Route path="/" element={<AdminContractorTab />} />
+          <Route path="/profile/:contractorId" element={<Profile />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
